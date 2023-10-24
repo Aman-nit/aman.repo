2dcdbc53479579915a5201f85d21ac536c6f1baa
Aman-nit <143195620+Aman-nit@users.noreply.github.com>
2023-10-24 13:18:49 +0530

    Delete vscode directory

--- vscode/OPERATOR.C
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-printf("%f", 3%2);
-    return 0;
-}
--- vscode/new.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-float a;
-printf("Enter the length of a side of square\n");
-scanf("%f", &a);
-printf("Area of a square is : %f", a*a);
-    return 0;
-} 
